696ba629b344187bb58455235b6aa822fc583a8e
Peter Jansen <petejan@users.noreply.github.com>
2012-04-17 09:24:38 +0000

    test commit

--- ABOS/src/org/imos/abos/dbms/Instrument.java
+++ ABOS/src/org/imos/abos/dbms/Instrument.java
@@ -1,7 +1,7 @@
 /*
  * IMOS data delivery project
  * Written by Peter Wiley
- * This code is copyright (c) Peter Wiley 2000 - ?
+ * This code is copyright (c) Peter Wiley 2000 - 2012
  * It is made available under the BSD Software Licence in the hope that it may be useful.
  * It has NO WARRANTY OF FITNESS OR SUITABILITY FOR ANY PURPOSE.
  * Feel free to fix any bugs that you may find.
