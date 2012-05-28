76e6c56b9a2585da5f88a606dd03b04d072804b0
peter.jansen@csiro.au <peter.jansen@csiro.au@3fccb8f3-8e94-1b9f-2bbe-cc910951128f>
2012-05-28 06:12:10 +0000

    Include instrument make/model/serial number in header

--- trunk/ABOS/sql/getWaterTemp.sql
+++ trunk/ABOS/sql/getWaterTemp.sql
@@ -1,23 +1,26 @@
-SELECT p || '-' || TRIM(to_char(d, '9999')) FROM
-(	SELECT DISTINCT parameter_code AS p, depth AS d
-		FROM raw_instrument_data 
+\a
+\t on
+\f ','
+SELECT p || '-' || TRIM(to_char(d, '9999')) || '-' || make || '-' || model || '-' || serial_number FROM
+(	SELECT DISTINCT parameter_code AS p, depth AS d, instrument_id, make, model, serial_number
+		FROM raw_instrument_data JOIN instrument USING (instrument_id)
 		WHERE mooring_id = 'PULSE_7' AND (parameter_code = 'WATER_TEMP') 
-		ORDER BY 2, 1
-) As t;
+		ORDER BY 2, 1, 3 
+) AS t;
 		
-SELECT * from CROSSTAB('SELECT DATE_TRUNC(''hour'', data_timestamp) AT time zone ''UTC'', parameter_code || ''-'' || TRIM(to_char(depth, ''9999'')), AVG(parameter_value) 
+SELECT * from CROSSTAB('SELECT DATE_TRUNC(''hour'', data_timestamp) AT time zone ''utc'', parameter_code || ''-'' || TRIM(to_char(depth, ''9999'')), AVG(parameter_value) 
 					           FROM raw_instrument_data 
 							   WHERE mooring_id = ''PULSE_7'' AND (parameter_code = ''WATER_TEMP'')
 							   GROUP BY DATE_TRUNC(''hour'', data_timestamp), parameter_code || ''-'' || TRIM(to_char(depth, ''9999'')) 
 						       ORDER BY 1, 2',
-					   'SELECT p || ''-'' || TRIM(to_char(d, ''9999'')) FROM
-(
-	SELECT DISTINCT parameter_code AS p, depth AS d
-		FROM raw_instrument_data 
-		WHERE mooring_id = ''PULSE_7'' AND (parameter_code = ''WATER_TEMP'') 
-		ORDER BY 2, 1
-) AS t') 
-	    AS ct( t timestamp, 
+					   'SELECT p || ''-'' || TRIM(to_char(d, ''9999'')) || ''-'' || TRIM(to_char(i, ''999999'')) FROM
+						(
+							SELECT DISTINCT parameter_code AS p, depth AS d, instrument_id AS i
+								FROM raw_instrument_data 
+								WHERE mooring_id = ''PULSE_7'' AND (parameter_code = ''WATER_TEMP'') 
+								ORDER BY 2, 1, 3
+						) AS t') 
+	    AS ct( t timestamp without time zone, 
 			   t1 numeric, 
 			   t2 numeric, 
 			   t3 numeric, 
