e75caacdb67bcb335c4d507db3e8e231e33fdc39
Peter Jansen <petejan@users.noreply.github.com>
2013-03-05 03:25:25 +0000

    Add parser for some SOFS-2 data, also NetCDF generator

---
+++ ABOS/src/org/imos/abos/netcdf/sofs2/Sofs2NetCDFCreator.java
@@ -0,0 +1,98 @@
+/*
+ * This code is copyright Peter Wiley 1998-2008
+ * It is made available under the BSD Software Licence in the hope that it may be useful.
+ * It has NO WARRANTY OF FITNESS OR SUITABILITY FOR ANY PURPOSE.
+ * Feel free to fix any bugs that you may find.
+ *
+ */
+
+package org.imos.abos.netcdf.sofs2;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.imos.abos.netcdf.BaseNetCDFCreator;
+import org.wiley.core.Common;
+
+/**
+ *
+ * @author peter
+ */
+public class Sofs2NetCDFCreator extends BaseNetCDFCreator
+{
+    private static Logger logger = Logger.getLogger(Sofs2NetCDFCreator.class.getName());
+
+    public void Sofs2NetCDFCreator()
+    {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String args[])
+    {
+        String $HOME = System.getProperty("user.home");
+
+        if(args.length == 0)
+        {
+            PropertyConfigurator.configure("log4j.properties");
+            Common.build("ABOS.conf");
+        }
+
+        Sofs2NetCDFCreator cdf = new Sofs2NetCDFCreator();
+
+        cdf.createTimeArray("SOFS-2");
+        cdf.createDepthArray("SOFS-2");
+        cdf.createCDFFile();
+    }
+
+    @Override
+    protected String getFileName()
+    {
+        SimpleDateFormat nameFormatter = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+        nameFormatter.setTimeZone(tz);
+
+        String filename = //System.getProperty("user.home")
+                        //+ "/"
+                        Sofs2Constants.PREFIX
+                        + Sofs2Constants.FACILITY_CODE
+                        + Sofs2Constants.DATA_CODE
+                        + Sofs2Constants.START_DATE
+                        + nameFormatter.format(startTime)
+                        + "_" + Sofs2Constants.platform_code + "_"
+                        + Sofs2Constants.FILE_VERSION
+                        + Sofs2Constants.deployment_code
+                        + "_"
+                        + Sofs2Constants.PRODUCT_TYPE
+                        + Sofs2Constants.END_DATE
+                        + nameFormatter.format(endTime)
+                        + Sofs2Constants.CREATION_DATE
+                        + nameFormatter.format(System.currentTimeMillis())
+                        + Sofs2Constants.SUFFIX
+                        ;
+
+        return filename;
+    }
+
+    @Override
+    protected void writeMooringSpecificAttributes()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd\'T\'HH:mm:ss\'Z\'");
+        df.setTimeZone(tz);
+                
+        dataFile.addGlobalAttribute("level", Sofs2Constants.level);
+        
+        dataFile.addGlobalAttribute("field_trip_id", Sofs2Constants.field_trip_id);
+        dataFile.addGlobalAttribute("field_trip_description", Sofs2Constants.field_trip_description);
+        
+        dataFile.addGlobalAttribute("site_code", Sofs2Constants.site_code);
+        dataFile.addGlobalAttribute("platform_code", Sofs2Constants.platform_code);
+        dataFile.addGlobalAttribute("deployment_code", Sofs2Constants.deployment_code);
+
+        dataFile.addGlobalAttribute("title", Sofs2Constants.title);
+        dataFile.addGlobalAttribute("abstract", Sofs2Constants.Abstract);
+        dataFile.addGlobalAttribute("keywords", Sofs2Constants.keywords);
+
+        dataFile.addGlobalAttribute("geospatial_vertical_min", 0);
+        dataFile.addGlobalAttribute("geospatial_vertical_max", Sofs2Constants.geospatial_vertical_max);        
+    }
+}
---
+++ ABOS/src/org/imos/abos/parsers/NameValueParser.java
@@ -0,0 +1,119 @@
+/*
+ * IMOS data delivery project
+ * Written by Peter Wiley
+ * This code is copyright (c) Peter Wiley 2000 - ?
+ * It is made available under the BSD Software Licence in the hope that it may be useful.
+ * It has NO WARRANTY OF FITNESS OR SUITABILITY FOR ANY PURPOSE.
+ * Feel free to fix any bugs that you may find.
+ */
+
+package org.imos.abos.parsers;
+
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import org.imos.abos.dbms.RawInstrumentData;
+
+/**
+ *
+ * @author peter
+ */
+public class NameValueParser extends AbstractDataParser
+{
+
+    @Override
+    protected boolean isHeader(String dataLine)
+    {
+        /*
+         * data lines have the format
+         *  2011-11-08 22:12:44 INFO: 7 done time 97 ,BV=15.3918 ,PT=16.88542 ,OBP=29.79 ,OT=16.2 ,CHL=46 ,NTU=173 PAR=0.7660786 ,meanAccel=-9.765544 ,meanLoad=46.95232
+         * anything else is a header
+         */
+        char c = dataLine.charAt(0);
+        if(! Character.isDigit(c) )
+        {
+            //
+            // it is a header
+            //
+            return true;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    protected void parseHeader(String dataLine) throws ParseException, NoSuchElementException
+    {
+        //
+        // don't care about the headers
+        //
+    }
+
+    @Override
+    protected void parseData(String dataLine) throws ParseException, NoSuchElementException
+    {
+        SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        String dateString;
+        Timestamp dataTimestamp = null;
+        String sName;
+        String sValue;
+        double value;
+        HashMap<String, String> hm = new HashMap(); 
+        hm.put("OBP", "OPTODE_BPHASE_VOLT");
+        hm.put("OT", "OPTODE_TEMP_VOLT");
+
+        StringTokenizer st = new StringTokenizer(dataLine, ",");
+        try
+        {
+            dateString = st.nextToken();
+            try
+            {
+                java.util.Date d = dateParser.parse(dateString);
+                dataTimestamp = new Timestamp(d.getTime());
+            }
+            catch(ParseException pex)
+            {
+                throw new ParseException("Timestamp parse failed for text '" + dateString + "'",0);
+            }
+
+            while(st.hasMoreElements())
+            {
+                String nvp = st.nextToken();
+                StringTokenizer nv = new StringTokenizer(nvp, "=");
+                sName = nv.nextToken();
+                if (hm.get(sName) != null)
+                {
+                    sValue = nv.nextToken();
+                    value = new Double(sValue.trim());
+                    //
+                    // ok, we have parsed out the values we need, can now construct the raw data class
+                    //
+                    RawInstrumentData row = new RawInstrumentData();
+
+                    row.setDataTimestamp(dataTimestamp);
+                    row.setDepth(instrumentDepth);
+                    row.setInstrumentID(currentInstrument.getInstrumentID());
+                    row.setLatitude(currentMooring.getLatitudeIn());
+                    row.setLongitude(currentMooring.getLongitudeIn());
+                    row.setMooringID(currentMooring.getMooringID());
+                    row.setParameterCode(hm.get(sName));
+                    row.setParameterValue(value);
+                    row.setSourceFileID(currentFile.getDataFilePrimaryKey());
+                    row.setQualityCode("RAW");
+
+                    boolean ok = row.insert();
+                }
+            }
+        }
+        catch (NoSuchElementException nse)
+        {
+          throw nse;
+        }
+    }
+
+}
